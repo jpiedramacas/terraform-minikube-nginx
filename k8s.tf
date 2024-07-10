4c56bddb88836ff0ebc6121df2dcbd864d4a603f
[root@gio-devops]$ <jpiedramacas@gmail.com>
2024-07-10 20:29:16 +0200

    v1

---
+++ k8s.tf
@@ -0,0 +1,51 @@
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "k8s-ns-by-tf"
+  }
+}
+
+resource "kubernetes_deployment" "example" {
+  metadata {
+    name = "terraform-example"
+    labels = {
+      test = "MyExampleApp"
+    }
+    namespace = "k8s-ns-by-tf"
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        test = "MyExampleApp"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "MyExampleApp"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:1.21.6"
+          name  = "example"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
